b4086670d3035ded1592bca6f9993e8ceb804efe
tou-dev <tou.dev.12@gmail.com>
2026-09-04 21:33:16 +0100

    ci fist tf

--- backend.tf
+++ backend.tf
@@ -2,9 +2,14 @@ terraform {
   required_version = ">= 1.10.0"
 
   backend "s3" {
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    force_path_style            = true
+    skip_credentials_validation  = true
+    skip_metadata_api_check      = true
+    skip_region_validation       = true
+    skip_requesting_account_id   = true # Bypasses the AWS account ID check
+    force_path_style             = true
+
+    endpoints = {
+      s3 = "https://placeholder-endpoint-will-be-overridden-in-ci"
+    }
   }
 }
